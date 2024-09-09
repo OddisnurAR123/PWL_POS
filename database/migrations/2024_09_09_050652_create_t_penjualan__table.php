148f55126780c7cb2d175467f4faf220e46091f9
Oddis Nur Alifathur Razaaq <oddisnur@gmail.com>
2024-09-09 12:28:00 +0700

    Pertemuan 3 Praktikum 2.2

---
+++ database/migrations/2024_09_09_050652_create_t_penjualan__table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_penjualan', function (Blueprint $table) {
+            $table->id('penjualan_id'); // Primary Key
+            $table->unsignedBigInteger('user_id')->index(); // Foreign Key
+            $table->string('pembeli', 50); // Nama pembeli
+            $table->string('penjualan_kode', 20)->unique(); // Kode penjualan
+            $table->dateTime('penjualan_tanggal'); // Tanggal penjualan
+            $table->timestamps();
+
+            // Mendefinisikan foreign key constraint ke tabel pengguna
+            $table->foreign('user_id')->references('user_id')->on('m_user');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_penjualan');
+    }
+};
