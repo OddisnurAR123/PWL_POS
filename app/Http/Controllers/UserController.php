0c8cc82ab3793317e29f44c695bf372dd8efd3fc
Oddis Nur Alifathur Razaaq <oddisnur@gmail.com>
2024-09-23 09:36:56 +0700

    Pertemuan 4 Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,21 +11,34 @@ class UserController extends Controller
     public function index()
     {
         // Tambah data user dengan Eloquent Model
-        $data = [
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-            'level_id' => 2
-        ];
-        UserModel::create($data); // Tambahkan data ke tabel m_user
+        // $data = [
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        // ];
+        // UserModel::create($data); // Tambahkan data ke tabel m_user
 
         // $data = [
         //     'nama' => 'Pelanggan Pertama',
         // ];
         // UserModel::where('username', 'customer-1')->update($data);
 
-        // Coba akses model UserModel
-        $user = UserModel::all(); // Ambil semua data dari tabel m_user
+        
+        // $user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::findOr(1, ['username', 'nama'], function (){
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function (){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
