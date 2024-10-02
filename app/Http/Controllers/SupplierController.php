fe73b8e549fc28d81b8dd44ef8d73228b4731498
Oddis Nur Alifathur Razaaq <oddisnur@gmail.com>
2024-10-03 00:41:57 +0700

    Pertemuan 6 Tugas

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\SupplierModel;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
 
 class SupplierController extends Controller
 {
@@ -28,27 +29,42 @@ class SupplierController extends Controller
     // Ambil data supplier dalam bentuk json untuk datatables
     public function list(Request $request)
     {
-        $supplier = SupplierModel::select('supplier_id', 'supplier_kode', 'supplier_nama', 'supplier_alamat', 'supplier_notlp');
+        // $supplier = SupplierModel::select('supplier_id', 'supplier_kode', 'supplier_nama', 'supplier_alamat', 'supplier_notlp');
 
-        // ftidak perlu ada filter pada supplier
+        // // ftidak perlu ada filter pada supplier
+        // // if ($request->supplier_id) {
+        // //     $supplier->where('supplier_id', $request->supplier_id);
+        // // }
+        
+        // return DataTables::of($supplier)
+        //     // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
+        //     ->addIndexColumn()
+        //     ->addColumn('aksi', function ($supplier) { // menambahkan kolom aksi
+        //         $btn = '<a href="' . url('/supplier/' . $supplier->supplier_id) . '" class="btn btn-info btn-sm">Detail</a> ';
+        //         $btn .= '<a href="' . url('/supplier/' . $supplier->supplier_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+        //         $btn .= '<form class="d-inline-block" method="POST" action="' .
+        //             url('/supplier/' . $supplier->supplier_id) . '">'
+        //             . csrf_field() . method_field('DELETE') .
+        //             '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm
+        //             (\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
+        //         return $btn;
+        //     })
+        //     ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
+        //     ->make(true);
+        $supplier = SupplierModel::select('supplier_id', 'supplier_kode', 'supplier_nama', 'supplier_alamat', 'supplier_notlp');
+        // Filter data supplier berdasarkan supplier_id
         // if ($request->supplier_id) {
         //     $supplier->where('supplier_id', $request->supplier_id);
         // }
-        
         return DataTables::of($supplier)
-            // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
-            ->addIndexColumn()
-            ->addColumn('aksi', function ($supplier) { // menambahkan kolom aksi
+            ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex) 
+            ->addColumn('aksi', function ($supplier) { // menambahkan kolom aksi 
                 $btn = '<a href="' . url('/supplier/' . $supplier->supplier_id) . '" class="btn btn-info btn-sm">Detail</a> ';
-                $btn .= '<a href="' . url('/supplier/' . $supplier->supplier_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
-                $btn .= '<form class="d-inline-block" method="POST" action="' .
-                    url('/supplier/' . $supplier->supplier_id) . '">'
-                    . csrf_field() . method_field('DELETE') .
-                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm
-                    (\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
+                $btn .= '<button onclick="modalAction(\'' . url('/supplier/' . $supplier->supplier_id . '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
+                $btn .= '<button onclick="modalAction(\'' . url('/supplier/' . $supplier->supplier_id . '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button> ';
                 return $btn;
             })
-            ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
+            ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html 
             ->make(true);
     }
 
@@ -123,13 +139,13 @@ class SupplierController extends Controller
             'supplier_kode'=> 'required|string|min:3|max:10|unique:m_supplier,supplier_kode,'. $id . ',supplier_id',
             'supplier_nama'=> 'required|string|max:100', // nama harus diisi, berupa string, dan maksimal 100 karakter
             'supplier_alamat'=> 'required|string|max:100', //nama harus diisi, berupa string, dan maksimal 100 karakter
-            'supplier_notelp' => 'nullable|string|max:15'
+            'supplier_notlp' => 'nullable|string|max:15'
         ]);
         SupplierModel::find($id)->update([
             'supplier_kode'  => $request->supplier_kode,
             'supplier_nama'  => $request->supplier_nama,
             'supplier_alamat'=> $request->supplier_alamat,
-            'supplier_notelp' => $request->supplier_notlp
+            'supplier_notlp' => $request->supplier_notlp
         ]);
         return redirect('/supplier')->with("success", "Data supplier berhasil diubah");
     }
@@ -151,4 +167,109 @@ class SupplierController extends Controller
             return redirect('/supplier')->with('error', 'Data Supplier gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
         }
     }
+
+    public function create_ajax()
+    {
+        return view('supplier.create_ajax');
+    }
+
+    public function store_ajax(Request $request)
+    {
+        // cek apakah request berupa ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'supplier_kode'    => 'required|string|min:3|unique:m_supplier,supplier_kode',
+                'supplier_nama'    => 'required|string|max:100',
+                'supplier_alamat' => 'required|string|max:100',
+                'supplier_notlp' => 'nullable|string|max:15'
+            ];
+            // use Illuminate\Support\Facades\Validator;
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status'    => false, // response status, false: error/gagal, true: berhasil
+                    'message'   => 'Validasi Gagal',
+                    'msgField'  => $validator->errors(), // pesan error validasi
+                ]);
+            }
+            SupplierModel::create($request->all());
+            return response()->json([
+                'status'    => true,
+                'message'   => 'Data supplier berhasil disimpan'
+            ]);
+        }
+        redirect('/');
+    }
+
+    // Menampilkan halaman form edit supplier ajax
+    public function edit_ajax(string $id)
+    {
+        $supplier = SupplierModel::find($id);
+        return view('supplier.edit_ajax', ['supplier' => $supplier]);
+    }
+
+    public function update_ajax(Request $request, $id)
+    {
+        // cek apakah request dari ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'supplier_kode' => 'required|max:20|unique:m_supplier,supplier_kode,' . $id . ',supplier_id',
+                'supplier_nama' => 'required|max:100', // nama harus diisi, berupa string, dan maksimal 100 karakter
+                'supplier_alamat' => 'required|string|max:100',
+                'supplier_notlp' => 'nullable|string|max:15'
+            ];
+            // use Illuminate\Support\Facades\Validator;
+            $validator = Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false, // respon json, true: berhasil, false: gagal
+                    'message' => 'Validasi gagal.',
+                    'msgField' => $validator->errors() // menunjukkan field mana yang error
+                ]);
+            }
+            $check = SupplierModel::find($id);
+            if ($check) {
+                $check->update($request->all());
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil diupdate'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
+    }
+
+    public function confirm_ajax(string $id)
+    {
+        $supplier = SupplierModel::find($id);
+        return view('supplier.confirm_ajax', ['supplier' => $supplier]);
+    }
+
+    public function delete_ajax(Request $request, $id)
+    {
+        // cek apakah request dari ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $supplier = SupplierModel::find($id);
+            if ($supplier) {
+                $supplier->delete();
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil dihapus'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\SupplierController;
-use App\Http\Controllers\StockController;
+use App\Http\Controllers\StokController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -57,9 +57,15 @@ Route::group(['prefix' => 'level'], function () {
     Route::post('/list', [LevelController::class, 'list']);      // menampilkan data level dalam bentuk json untuk datatables
     Route::get('/create', [LevelController::class, 'create']);   // menampilkan halaman form tambah level
     Route::post('/', [LevelController::class, 'store']);         // menyimpan data level baru
+    Route::get('/create_ajax', [LevelController::class, 'create_ajax']); // Menampilkan halaman form tambah level Ajax
+    Route::post('/ajax', [LevelController::class, 'store_ajax']); // Menyimpan data level baru Ajax
     Route::get('/{id}', [LevelController::class, 'show']);       // menampilkan detail level
     Route::get('/{id}/edit', [LevelController::class, 'edit']);  // menampilkan halaman form edit level
     Route::put('/{id}', [LevelController::class, 'update']);     // menyimpan perubahan data level
+    Route::get('/{id}/edit_ajax', [LevelController::class, 'edit_ajax']);  // menampilkan halaman form edit level Ajax
+    Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']); // menyimpan perubahan data level Ajax
+    Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']);     // menyimpan perubahan data level Ajax
+    Route::delete('/{id}/delete_ajax', [LevelController::class, 'delete_ajax']); // menghapus data level Ajax
     Route::delete('/{id}', [LevelController::class, 'destroy']); // menghapus data level
 });
 
@@ -68,9 +74,15 @@ Route::group(['prefix' => 'kategori'], function () {
     Route::post('/list', [KategoriController::class, 'list']);      // menampilkan data kategori dalam bentuk json untuk datatables
     Route::get('/create', [KategoriController::class, 'create']);   // menampilkan halaman form tambah kategori
     Route::post('/', [KategoriController::class, 'store']);         // menyimpan data kategori baru
+    Route::get('/create_ajax', [KategoriController::class, 'create_ajax']); // Menampilkan halaman form tambah kategori Ajax
+    Route::post('/ajax', [KategoriController::class, 'store_ajax']); // Menyimpan data kategori baru Ajax
     Route::get('/{id}', [KategoriController::class, 'show']);       // menampilkan detail kategori
     Route::get('/{id}/edit', [KategoriController::class, 'edit']);  // menampilkan halaman form edit kategori
     Route::put('/{id}', [KategoriController::class, 'update']);     // menyimpan perubahan data kategori
+    Route::get('/{id}/edit_ajax', [KategoriController::class, 'edit_ajax']);  // menampilkan halaman form edit kategori Ajax
+    Route::put('/{id}/update_ajax', [KategoriController::class, 'update_ajax']); // menyimpan perubahan data kategori Ajax
+    Route::get('/{id}/delete_ajax', [KategoriController::class, 'confirm_ajax']);     // menyimpan perubahan data kategori Ajax
+    Route::delete('/{id}/delete_ajax', [KategoriController::class, 'delete_ajax']); // menghapus data kategori Ajax
     Route::delete('/{id}', [KategoriController::class, 'destroy']); // menghapus data kategori
 });
 
@@ -79,9 +91,15 @@ Route::group(['prefix' => 'supplier'], function () {
     Route::post('/list', [SupplierController::class, 'list']);      // menampilkan data supplier dalam bentuk json untuk datatables
     Route::get('/create', [SupplierController::class, 'create']);   // menampilkan halaman form tambah supplier
     Route::post('/', [SupplierController::class, 'store']);         // menyimpan data supplier baru
+    Route::get('/create_ajax', [SupplierController::class, 'create_ajax']); // Menampilkan halaman form tambah supllier Ajax
+    Route::post('/ajax', [SupplierController::class, 'store_ajax']); // Menyimpan data supllier baru Ajax
     Route::get('/{id}', [SupplierController::class, 'show']);       // menampilkan detail supplier
     Route::get('/{id}/edit', [SupplierController::class, 'edit']);  // menampilkan halaman form edit supplier
     Route::put('/{id}', [SupplierController::class, 'update']);     // menyimpan perubahan data supplier
+    Route::get('/{id}/edit_ajax', [SupplierController::class, 'edit_ajax']);  // menampilkan halaman form edit supllier Ajax
+    Route::put('/{id}/update_ajax', [SupplierController::class, 'update_ajax']); // menyimpan perubahan data supllier Ajax
+    Route::get('/{id}/delete_ajax', [SupplierController::class, 'confirm_ajax']);     // menyimpan perubahan data supllier Ajax
+    Route::delete('/{id}/delete_ajax', [SupplierController::class, 'delete_ajax']); // menghapus data supllier Ajax
     Route::delete('/{id}', [SupplierController::class, 'destroy']); // menghapus data supplier
 });
 
@@ -90,19 +108,31 @@ Route::group(['prefix' => 'barang'], function () {
     Route::post('/list', [BarangController::class, 'list']);      // menampilkan data barang dalam bentuk json untuk datatables
     Route::get('/create', [BarangController::class, 'create']);   // menampilkan halaman form tambah barang
     Route::post('/', [BarangController::class, 'store']);         // menyimpan data barang baru
+    Route::get('/create_ajax', [BarangController::class, 'create_ajax']); // Menampilkan halaman form tambah barang Ajax
+    Route::post('/ajax', [BarangController::class, 'store_ajax']); // Menyimpan data barang baru Ajax
     Route::get('/{id}', [BarangController::class, 'show']);       // menampilkan detail barang
     Route::get('/{id}/edit', [BarangController::class, 'edit']);  // menampilkan halaman form edit barang
     Route::put('/{id}', [BarangController::class, 'update']);     // menyimpan perubahan data barang
+    Route::get('/{id}/edit_ajax', [BarangController::class, 'edit_ajax']);  // menampilkan halaman form edit barang Ajax
+    Route::put('/{id}/update_ajax', [BarangController::class, 'update_ajax']); // menyimpan perubahan data barang Ajax
+    Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);     // menyimpan perubahan data barang Ajax
+    Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']); // menghapus data barang Ajax
     Route::delete('/{id}', [BarangController::class, 'destroy']); // menghapus data barang
 });
 
-Route::group(['prefix' => 'stock'], function () {
-    Route::get('/', [StockController::class, 'index']);          // menampilkan halaman awal stok
-    Route::post('/list', [StockController::class, 'list']);      // menampilkan data stok dalam bentuk json untuk datatables
-    Route::get('/create', [StockController::class, 'create']);   // menampilkan halaman form tambah stok
-    Route::post('/', [StockController::class, 'store']);         // menyimpan data stok baru
-    Route::get('/{id}', [StockController::class, 'show']);       // menampilkan detail stok
-    Route::get('/{id}/edit', [StockController::class, 'edit']);  // menampilkan halaman form edit stok
-    Route::put('/{id}', [StockController::class, 'update']);     // menyimpan perubahan data stok
-    Route::delete('/{id}', [StockController::class, 'destroy']); // menghapus data stok
+Route::group(['prefix' => 'stok'], function () {
+    Route::get('/', [StokController::class, 'index']);          // menampilkan halaman awal stok
+    Route::post('/list', [StokController::class, 'list']);      // menampilkan data stok dalam bentuk json untuk datatables
+    Route::get('/create', [StokController::class, 'create']);   // menampilkan halaman form tambah stok
+    Route::post('/', [StokController::class, 'store']);         // menyimpan data stok baru
+    Route::get('/create_ajax', [StokController::class, 'create_ajax']); // Menampilkan halaman form tambah stok Ajax
+    Route::post('/ajax', [StokController::class, 'store_ajax']); // Menyimpan data stok baru Ajax
+    Route::get('/{id}', [StokController::class, 'show']);       // menampilkan detail stok
+    Route::get('/{id}/edit', [StokController::class, 'edit']);  // menampilkan halaman form edit stok
+    Route::put('/{id}', [StokController::class, 'update']);     // menyimpan perubahan data stok
+    Route::get('/{id}/edit_ajax', [StokController::class, 'edit_ajax']);  // menampilkan halaman form edit stok Ajax
+    Route::put('/{id}/update_ajax', [StokController::class, 'update_ajax']); // menyimpan perubahan data stok Ajax
+    Route::get('/{id}/delete_ajax', [StokController::class, 'confirm_ajax']);     // menyimpan perubahan data stok Ajax
+    Route::delete('/{id}/delete_ajax', [StokController::class, 'delete_ajax']); // menghapus data stok Ajax
+    Route::delete('/{id}', [StokController::class, 'destroy']); // menghapus data stok
 });
